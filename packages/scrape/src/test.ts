b4839d1970da1852ae5a9fdfd750d6af146d244f
Thans <ntbperst@outlook.com>
2025-04-25 02:13:26 +0800

    chore: remove unused apps/docs and apps/web directories; update .gitignore and package.json for scrape

--- packages/scrape/src/test.ts
+++ packages/scrape/src/test.ts
@@ -1,28 +1,29 @@
 import { randomUUID } from "node:crypto";
 import { QueueManager } from "./queue/index.js";
 
+
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://crawlee.dev'
+    url: 'https://docs.bullmq.io/guide/returning-job-data',
+    engine: 'cheerio'
 });
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://docs.bullmq.io/guide/returning-job-data'
+    url: 'https://crawlee.dev/js/docs/introduction/refactoring',
+    engine: 'cheerio'
 });
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://crawlee.dev/js/docs/guides/configuration'
+    url: 'https://turbo.build/repo/docs/crafting-your-repository/creating-an-internal-package',
+    engine: 'cheerio'
 });
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://crawlee.dev/js/docs/introduction/refactoring'
+    url: 'https://docs.bullmq.io/guide/workers/sandboxed-processors',
+    engine: 'cheerio'
 });
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://turbo.build/repo/docs/crafting-your-repository/creating-an-internal-package'
+    url: 'https://docs.bullmq.io/guide/workers/auto-removal-of-jobs',
+    engine: 'cheerio'
 });
 await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://docs.bullmq.io/guide/workers/sandboxed-processors'
-});
-await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://docs.bullmq.io/guide/workers/auto-removal-of-jobs'
-});
-await QueueManager.getInstance().addJob('scrape', 'scrape', randomUUID(), {
-    url: 'https://www.baidu.com'
+    url: 'https://www.producthunt.com/',
+    engine: 'cheerio'
 });
 process.exit(0);
