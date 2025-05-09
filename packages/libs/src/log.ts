020f91fdc4cef8004a5a42b21b3b029f09258d48
Thans <ntbperst@outlook.com>
2025-05-10 00:51:51 +0800

    ✨ feat: update package.json to add testing scripts and dependencies, and modify logging format in log.ts for improved clarity

--- packages/libs/src/log.ts
+++ packages/libs/src/log.ts
@@ -2,11 +2,11 @@ import { log } from "crawlee";
 
 class ConsoleStream {
   write(line: string) {
-    log.info(`\x1b[33mAPI: ${line}\x1b[0m`);
+    log.info(`\x1b[33mAPI:\x1b[0m ${line}`);
   }
 
   error(line: string) {
-    log.error(`\x1b[31mAPI: ${line}\x1b[0m`);
+    log.error(`\x1b[31mAPI:\x1b[0m ${line}`);
   }
 }
 export { log, ConsoleStream };
