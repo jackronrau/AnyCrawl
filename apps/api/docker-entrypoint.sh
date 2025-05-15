020eb13f4d8e36299f2ea493ea12d5c7e9f65c3c
Thans <ntbperst@outlook.com>
2025-05-15 16:26:25 +0800

    ✨ chore(docker): fix database migration path in docker-entrypoint.sh for improved initialization on first startup.

--- apps/api/docker-entrypoint.sh
+++ apps/api/docker-entrypoint.sh
@@ -3,10 +3,10 @@ set -e
 
 MIGRATE_DATABASE=false
 FIRST_TIME_MIGRATION="FIRST_TIME_MIGRATION_$NODE_ENV"
-if [[ ! -e /usr/src/app/db/$FIRST_TIME_MIGRATION ]] || [[ $MIGRATE_DATABASE = true ]]; then
+if [[ ! -e /usr/src/app/$FIRST_TIME_MIGRATION ]] || [[ $MIGRATE_DATABASE = true ]]; then
     # Place your script that you only want to run on first startup.
     echo "Initialize database first time only"
-    touch /usr/src/app/db/$FIRST_TIME_MIGRATION
+    touch /usr/src/app/$FIRST_TIME_MIGRATION
     pnpm drizzle-kit migrate & PID=$!
     # Wait for migration to finish
     wait $PID
